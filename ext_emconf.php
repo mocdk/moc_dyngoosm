e161531f86a10ba2ea99b38e6d28ae3ec62e1595
www-data <www-data@mocsystems.com>
2011-11-02 18:30:46 +0100

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "moc_dyngoosm".
+#
+# Auto generated 21-02-2011 16:10
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'MOC Dynamic Google Sitemap',
+	'description' => 'Create a Google Sitemap by defining a table containing dynamic content(pages)',
+	'category' => 'plugin',
+	'author' => 'Mikkel H. Henriksen',
+	'author_email' => 'mikkel@mocsystems.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"6e73";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"3341";s:14:"ext_tables.php";s:4:"083d";s:19:"flexform_ds_pi1.xml";s:4:"101d";s:16:"locallang_db.xml";s:4:"e8b4";s:19:"doc/wizard_form.dat";s:4:"ed3a";s:20:"doc/wizard_form.html";s:4:"f949";s:32:"pi1/class.tx_mocdyngoosm_pi1.php";s:4:"d9f4";s:17:"pi1/locallang.xml";s:4:"acef";s:13:"pi1/mdgsm.xml";s:4:"c9be";s:26:"static/mdgsm/constants.txt";s:4:"d41d";s:22:"static/mdgsm/setup.txt";s:4:"58a5";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ pi1/class.tx_mocdyngoosm_pi1.php
@@ -0,0 +1,188 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 Mikkel H. Henriksen <mikkel@mocsystems.com>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ * Hint: use extdeveval to insert/update function index above.
+ */
+
+require_once(PATH_tslib.'class.tslib_pibase.php');
+
+
+/**
+ * Plugin 'MOC Dynamic Google Sitemap' for the 'moc_dyngoosm' extension.
+ *
+ * @author	Mikkel H. Henriksen <mikkel@mocsystems.com>
+ * @package	TYPO3
+ * @subpackage	tx_mocdyngoosm
+ */
+class tx_mocdyngoosm_pi1 extends tslib_pibase {
+	var $prefixId      = 'tx_mocdyngoosm_pi1';		// Same as class name
+	var $scriptRelPath = 'pi1/class.tx_mocdyngoosm_pi1.php';	// Path to this script relative to the extension dir.
+	var $extKey        = 'moc_dyngoosm';	// The extension key.
+
+	/**
+	 * The main method of the PlugIn
+	 *
+	 * @param	string		$content: The PlugIn content
+	 * @param	array		$conf: The PlugIn configuration
+	 * @return	The content that is displayed on the website
+	 */
+	function main($content, $conf) {
+		$this->conf = $conf;
+		$this->pi_setPiVarDefaults();
+		$this->pi_loadLL();
+		$this->pi_USER_INT_obj = 1;	// Configuring so caching is not expected. This value means that no cHash params are ever set. We do this, because it's a USER_INT object!
+
+		$this->pi_initPIflexForm();
+		$this->errorWrapper = '<html><head><title>Error in plugin: tx_mocdyngoosm</title></head><body><h3>An exception is thrown by tx_mocdyngoosm with the following message:</h3><p>###ERROR_MSG###</p></body></html>';
+		$this->is_index = (intval($this->pi_getFFvalue($this->cObj->data['pi_flexform'],'display_type','sDEF')) === 1) || (intval($this->conf['is_index'])===1);
+		try{
+			$this->xml = '<?xml version="1.0" encoding="UTF-8"?>'.chr(10);
+			$this->xml .= $this->generateXML();
+		}
+		catch(Exception $e){
+			exit(str_replace('###ERROR_MSG###',$e->getMessage(), $this->errorWrapper));
+		}
+
+		ob_start();
+		header('Content-type: text/xml');
+		print($this->xml);
+		ob_flush();
+		exit();
+	}
+
+	private function generateXML(){
+		if($this->is_index){
+			$this->mapList = t3lib_div::trimExplode(chr(10),$this->pi_getFFvalue($this->cObj->data['pi_flexform'],'links_to_sitemaps','sIndex'),true);
+			if(count($this->mapList) === 0){
+				$this->mapList = t3lib_div::trimExplode('||',$this->conf['siteMaps'],true);
+				if(count($this->mapList) === 0){
+					throw new Execption('No sitemaps defined for the index!');
+				}
+			}
+			return $this->getIndexXML();
+
+		}else{
+			try{
+				$this->getConf();
+				$this->pageArr = $this->getPages();
+				return $this->getSitemapXML();
+			}
+			catch(Exception $e){
+				throw new Exception('Could not get configuration: '.$e->getMessage());
+			}
+		}
+	}
+
+	private function getIndexXML(){
+		$xml .= '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'.chr(10);
+		foreach($this->mapList as $sitemap){
+			$lastmod = $this->getLastModified($sitemap);
+			$xml .= '<sitemap><loc>'.t3lib_div::locationHeaderUrl(htmlentities($sitemap)).'</loc><lastmod>'.$lastmod.'</lastmod></sitemap>';
+		}
+		$xml .= chr(10).'</sitemapindex>';
+		return $xml;
+	}
+
+	private function getSitemapXML(){
+		$xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'.chr(10);
+		foreach($this->pageArr as $page){
+			$xml .= '<url>';
+			$xml .= '<loc>'.t3lib_div::locationHeaderUrl(htmlentities($page['url'])).'</loc>';
+			$xml .= '<lastmod>'.date('c',$page['lastmod']).'</lastmod>';
+			$xml .= '<priority>'.$this->priority.'</priority>';
+			$xml .= '</url>'.chr(10);
+		}
+		$xml .= '</urlset>';
+		return $xml;
+	}
+
+	private function getPages(){
+		$pagearr = array();
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('uid,'.$this->lastmodField,$this->pageTable,'pid='.$this->storagePid.' '.$this->additionalWhere.' '.$this->cObj->enableFields($this->pageTable),'',$this->lastmodField.' DESC');
+		while($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)){
+			$url = $this->pi_getPageLink($this->singlePid,'' ,array($this->piVar_identifier=>$row['uid']));
+			$pagearr[] = array('url'=>$url,'lastmod'=>$row['tstamp']);
+		}
+
+		return $pagearr;
+	}
+
+	private function getLastModified($url){
+		$lastmod = 0;
+		$xmlreader = new XMLReader();
+		if(!@$xmlreader->open($url)){
+			throw new Exception('Could not open the sitemap defined: '.$url);
+		}
+		while($xmlreader->read()){
+			if($xmlreader->nodeType == XMLReader::ELEMENT){
+				if($xmlreader->name == 'lastmod'){
+					$xmlreader->read();
+					$tmp = strtotime($xmlreader->value);
+					if($tmp > $lastmod){
+						$lastmod = $tmp;
+					}
+				}
+			}
+		}
+		$lastmod = date('c',$lastmod);
+		return $lastmod;
+	}
+
+	private function getConf(){
+		$ffFields = array
+		(
+			'pageTable'=>'table_of_records',
+			'storagePid'=>'storage_pid',
+			'singlePid'=>'pid_singleview',
+			'priority'=>'priority',
+			'piVar_identifier' => 'pivar_identifier',
+			'lastmodField'=>'lastmod_field'
+
+		);
+		$intvals = array('storagePid','singlePid');
+		foreach($ffFields as $property=>$field){
+			$this->$property = trim($this->pi_getFFvalue($this->cObj->data['pi_flexform'],$field,'sTable'));
+			if(strlen($this->$property)==0){
+				$this->$property = trim($this->conf[$property]);
+			}
+		}
+		if(strlen($this->pageTable)==0){
+			throw new Exception('No table for record given');
+		}
+		if(strlen($this->piVar_identifier)==0){
+			$this->piVar_identifier = 'tx_'.$this->pageTable.'_pi1[uid]';
+		}
+		$this->additionalWhere = mysql_real_escape_string(trim($this->conf['additionalWhere']));
+	}
+}
+
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/moc_dyngoosm/pi1/class.tx_mocdyngoosm_pi1.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/moc_dyngoosm/pi1/class.tx_mocdyngoosm_pi1.php']);
+}
+
+?>
